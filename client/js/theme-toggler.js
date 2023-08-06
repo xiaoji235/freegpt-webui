e78583960f17eedf82af215d19cc45b094ee738e
xjtx <73387366+xiaoji235@users.noreply.github.com>
2023-08-06 17:17:30 +0800

    Update theme-toggler.js

--- client/js/theme-toggler.js
+++ client/js/theme-toggler.js
@@ -21,16 +21,3 @@ function toggleTheme() {
 	switch_theme_toggler.checked = currentTheme === "theme-dark";
 })();
 
-
-
-if (localStorage.getItem('isVerified')) {
-} else {
-  var password = prompt('请输入访问码（私信小基获取访问码）：');
-  if (password === 'xiaoji') {
-    localStorage.setItem('isVerified', true);
-    alert('欢迎使用，此项目免费使用，请勿滥用！');
-  } else {
-    alert('密码错误，请重试！');
-location.reload()
-  }
-}
